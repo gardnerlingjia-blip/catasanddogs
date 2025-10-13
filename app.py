0cc1e780dfe8a5a08f6d3d2c99a506918a3f4f19
gardnerlingjia-blip <gardner.lingjia@gmail.com>
2025-10-13 14:44:22 +0200

    Update app.py

--- app.py
+++ app.py
@@ -11,8 +11,10 @@ st.title("Cat vs Dog Classifier (Batch Predictions)")
 uploaded_file = st.file_uploader("Upload an image", type=["jpg", "jpeg", "png"])
 
 if uploaded_file is not None:
+  
     image = Image.open(uploaded_file)
-    st.image(image, caption="Uploaded Image", use_column_width=True)
+    st.image(image, caption="Uploaded Image", use_container_width=True)
+
 
     filename = uploaded_file.name
     match = predictions_df[predictions_df["filename"] == filename]
@@ -22,6 +24,7 @@ if uploaded_file is not None:
         st.write(f"### Prediction: {label}")
     else:
         st.write("### No prediction found for this image in batch_predictions.csv.")
+
 
 
 
